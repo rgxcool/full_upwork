298b690ea352953e8a7e7298608af8cb8c8fe4fb
Slacker <cyrus.malekani@gmail.com>
2025-06-23 21:00:44 +0200

    Updated package.json. Fixed courseParser logic to correctly import educationData from EducationData.xlsx to the DB. Polished AddStudent.vue to dynamically look up Programs and Courses alog with fixing municiplaity as type etc...
    Fixed v-if in ExamOverview.

--- frontend/src/views/Exams/ExamOverview.vue
+++ frontend/src/views/Exams/ExamOverview.vue
@@ -1,32 +1,26 @@
 <template>
-    <div>
-
-      <div v-if="userRole === 'student'">
-
-        <StudentExamForm />
-      </div>
-
-
-      <div v-if="userRole === 'teacher' || userRole === 'systemadmin' || userRole === 'admin'">
-        <ExamAdminTable />
-      </div>
-      <!-- Om ingen giltig roll -->
-      <div >
-        <p>Du har inte behörighet att visa denna sida.</p>
-      </div>
+  <div>
+    <div v-if="userRole === 'student'">
+      <StudentExamForm />
     </div>
-  </template>
-  
-  <script setup>
 
-import { computed } from 'vue'
+    <div v-if="userRole === 'teacher' || userRole === 'systemadmin' || userRole === 'admin'">
+      <ExamAdminTable />
+    </div>
+    <!-- Om ingen giltig roll -->
+    <div v-else>
+      <p>Du har inte behörighet att visa denna sida.</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { computed } from 'vue'
   import { useStore } from 'vuex'
-  
+
   import StudentExamForm from '@/views/Exams/ExamForm.vue'
   import ExamAdminTable from '@/views/Exams/ExamAdminTable.vue'
 
   const store = useStore()
   const userRole = computed(() => store.getters.userRole || 'guest')
-
-  </script>
-  
+</script>
