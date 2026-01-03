2e3475b5ab41f3ee9f48e408f61c3fc59f1f412c
Martin Kanerva <martin@nexendo.com>
2026-01-03 02:26:42 +0100

    yes

--- vitest.config.js
+++ vitest.config.js
@@ -2,26 +2,17 @@ import { webcrypto } from "node:crypto";
 import { defineConfig } from "vitest/config";
 
 if (
-    !globalThis.crypto ||
-    typeof globalThis.crypto.getRandomValues !== "function"
+        !globalThis.crypto ||
+        typeof globalThis.crypto.getRandomValues !== "function"
 ) {
-    globalThis.crypto = webcrypto;
+        globalThis.crypto = webcrypto;
 }
-
-if (!process.env.MONGOMS_VERSION) {
-    process.env.MONGOMS_VERSION = "7.0.12";
-}
-
-if (!process.env.MONGOMS_DISTRO) {
-    process.env.MONGOMS_DISTRO = "ubuntu-20.04";
-}
-
 export default defineConfig({
-    test: {
-        environment: 'node',
-        exclude: ["frontend/**", "node_modules/**", "dist/**"],
-        include: ["backend/tests/**/*.test.js"],
-        maxWorkers: 12,
-        silent: true,
-    },
+        test: {
+                environment: 'node',
+                exclude: ["frontend/**", "node_modules/**", "dist/**"],
+                include: ["backend/tests/**/*.test.js"],
+                maxWorkers: 12,
+                silent: true,
+        },
 });
