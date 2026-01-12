acf3f057763b71c6f9fdef053a52bbeb52f10d8c
Martin Kanerva <martin@nexendo.com>
2026-01-12 20:21:53 +0100

    Meeting has createdBy set to user inviting the student

--- backend/src/models/Meeting.js
+++ backend/src/models/Meeting.js
@@ -2,27 +2,28 @@
 import mongoose from 'mongoose';
 
 const meetingSchema = new mongoose.Schema({
-  title: String,
-  start: Date,
-  location: String,
-  student: {
-    id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    personalNumber: String
-  },
-bookedBy: {
-  type: String,
-  enum: ['syv', 'specped', 'admin', 'systemadmin'],
-  required: true
-},
-  info: {
-    type: String,
-    default: ''
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
+    title: String,
+    start: Date,
+    location: String,
+    student: {
+        id: mongoose.Schema.Types.ObjectId,
+        name: String,
+        personalNumber: String
+    },
+    bookedBy: {
+        type: String,
+        enum: ['syv', 'specped', 'admin', 'systemadmin'],
+        required: true
+    },
+    info: {
+        type: String,
+        default: ''
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
 });
 
 export default mongoose.model("Meeting", meetingSchema);
